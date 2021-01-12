f232364b4f05f1f130fb72a26f3dfa6ca8919e35
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 23:08:27 +0100

    Added a form and basic javascript

---
+++ js/index.js
@@ -0,0 +1,37 @@
+function ipProbe() {
+    var ip = document.getElementById("ip").value;
+    
+}
+/*
+var slider = document.getElementById("myRange");
+        var output = document.getElementById("zoomerimg");
+        var mapboundries = document.getElementById("wowmap")
+        var zoomerimg = document.getElementById("zoomerimg")
+        var zoomer = document.getElementById("zoomer")
+        document.onmousemove = function(e) {
+            var x = e.clientX;
+            var y = e.clientY;
+            zoomer.style.marginLeft = x + "px"
+            zoomer.style.marginTop = y + "px"
+            y = y  + window.scrollY - mapboundries.offsetTop;
+            var translatex = x / mapboundries.clientWidth;
+            var translatey = y / mapboundries.clientHeight;
+
+            var translatex = translatex * zoomerimg.clientWidth-150
+            var translatey = translatey * zoomerimg.clientHeight-150
+            zoomerimg.style.transform = 'translateX(-' + translatex + 'px)'
+            zoomerimg.style.transform += 'translateY(-' + translatey + 'px)'
+
+        }
+        function mappress() {
+            if (zoomer.style.display == "inline") {
+                zoomer.style.display = "none"
+            } else {
+                zoomer.style.display = "inline"
+            }
+        }
+        slider.oninput = function() {
+            output.style.width = this.value*100 + "%";
+        }
+
+        */
